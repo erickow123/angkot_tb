52cfc81fa88a535343b6c87464301e0d4762c08f
unknown <erickoow@gmail.com>
2019-11-07 23:01:53 +0700

    first commit

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 	$host = "localhost";
 	$user = "postgres";
-	$pass = "root69";
+	$pass = "erickroot";
 	$port = "5432";
 	$dbname = "tb_angkot";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
